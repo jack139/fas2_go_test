01399a895315d46bb7f743af9efc63b976c30039
gt <jack139@gmail.com>
2022-08-31 16:16:00 +0800

    go test

--- convert2onnx/onnx_infer.py
+++ convert2onnx/onnx_infer.py
@@ -5,23 +5,24 @@ import cv2
 
 from model_lib.MiniFASNet import MiniFASNetV2, MiniFASNetV1SE
 
-fas2_model_1 = '../../face_model/fas_silent/2.7_80x80_MiniFASNetV2.pth'
-fas2_model_2 = '../../face_model/fas_silent/4_0_0_80x80_MiniFASNetV1SE.pth'
-
-
 
 def to_numpy(tensor):
     return tensor.detach().cpu().numpy() if tensor.requires_grad else tensor.cpu().numpy()
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-image = cv2.imread("../../yhfacelib/data/test/a1.png")
+image = cv2.imread("../data/aimg.jpg")
 img = cv2.resize(image, (80, 80))
+img = img[:, :, ::-1] # RGB GRB 转换
 print(img.shape)
+
+img = img.copy()
 
 img = torch.from_numpy(img.transpose((2, 0, 1)))
 img = img.float()
 img = img.unsqueeze(0).to(device)
+
+#print(img)
 
 # test run onnx model
 onnx_session= onnxruntime.InferenceSession("outputs/2.7_80x80_MiniFASNetV2.onnx")
---
+++ predict_onnx.go
@@ -0,0 +1,115 @@
+package main
+
+/*
+CGO_CPPFLAGS="-I/usr/local/include/opencv4" CGO_LDFLAGS="-L/usr/local/lib -lopencv_core -lopencv_calib3d -lopencv_imgproc" go build -o data/
+LD_LIBRARY_PATH=/usr/local/lib data/onnx_test
+*/
+
+import (
+	"log"
+	//"image"
+
+	"github.com/ivansuteja96/go-onnxruntime"
+	"github.com/disintegration/imaging"
+)
+
+const (
+	//test_image_path = "data/a3.png"
+	test_image_path = "data/aimg.jpg"
+
+	fas2Model1_path = "convert2onnx/outputs/2.7_80x80_MiniFASNetV2.onnx"
+	fas2Model2_path = "convert2onnx/outputs/4_0_0_80x80_MiniFASNetV1SE.onnx"
+
+	fas_model_input_size = 80
+)
+
+var (
+	fas2Model1 *onnxruntime.ORTSession
+	fas2Model2 *onnxruntime.ORTSession
+)
+
+func main() {
+	if err := loadModelWeight(); err!=nil {
+		log.Fatal("Load model fail: ", err.Error())
+	}
+
+	// load image
+	srcImage, err := imaging.Open(test_image_path)
+	if err != nil {
+		log.Fatal("Error: %s\n", err.Error())
+	}
+
+	shape1 := []int64{1, 3, fas_model_input_size, fas_model_input_size}
+	input1 := preprocessImage(srcImage, fas_model_input_size)
+
+	//log.Println(input1[:100])
+
+	// 模型1 检测
+	res, err := fas2Model1.Predict([]onnxruntime.TensorValue{
+		{
+			Value: input1,
+			Shape: shape1,
+		},
+	})
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	if len(res) == 0 {
+		log.Println("Failed get result")
+		return
+	}
+
+	predictionA1 := res[0].Value.([]float32)
+	predictionB1 := softmax(predictionA1)
+
+	log.Println("predictionA1:", predictionA1)
+	log.Println("predictionB1:", predictionB1)
+
+
+
+	// 模型2 检测
+	res2, err := fas2Model2.Predict([]onnxruntime.TensorValue{
+		{
+			Value: input1,
+			Shape: shape1,
+		},
+	})
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	if len(res2) == 0 {
+		log.Println("Failed get result")
+		return
+	}
+
+	predictionA2 := res2[0].Value.([]float32)
+	predictionB2 := softmax(predictionA2)
+
+	log.Println("predictionA2:", predictionA2)
+	log.Println("predictionB2:", predictionB2)
+
+	log.Println("Real Score: ", (predictionB1[1]+predictionB2[1])/2 )
+
+}
+
+
+func loadModelWeight() (err error) {
+	ortEnvDet := onnxruntime.NewORTEnv(onnxruntime.ORT_LOGGING_LEVEL_ERROR, "development")
+	ortDetSO := onnxruntime.NewORTSessionOptions()
+
+	fas2Model1, err = onnxruntime.NewORTSession(ortEnvDet, fas2Model1_path, ortDetSO)
+	if err != nil {
+		return err
+	}
+
+	fas2Model2, err = onnxruntime.NewORTSession(ortEnvDet, fas2Model2_path, ortDetSO)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
